0695effc117b01ca267a28c2c99f304bacdd1e6c
greisz <gabor.reisz@gmail.com>
2019-04-04 12:01:53 +0200

    Printing to the terminal window

---
+++ week-01/day-04/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+  public static void main(String[] args) {
+    System.out.println("Hello András!");
+    System.out.println("Hello Zsombor!");
+    System.out.println("Hello Zsófi!");
+  }
+}
