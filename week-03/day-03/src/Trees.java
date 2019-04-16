83e650c755fb76536d675d018ed98c75e33fb44a
greisz <gabor.reisz@gmail.com>
2019-04-16 22:29:34 +0200

    Trees

---
+++ week-03/day-03/src/Trees.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Trees {
+  public static void main(String[] args) {
+    List<String> treeType = new ArrayList<>(Arrays.asList("mulberry", "juniper", "ash", "oak", "juniper"));
+    List<String> leafColor = new ArrayList<>(Arrays.asList("green", "green", "green", "green", "green"));
+    List<Integer> treeAge = new ArrayList<>(Arrays.asList(12, 20, 5, 3, 8));
+    List<String> treeSex = new ArrayList<>(Arrays.asList("female", "male", "male", "monoecious", "female"));
+  }
+}
