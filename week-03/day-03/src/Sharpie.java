6f69ed7e2110398c6d532ac00921892c08d8cd23
greisz <gabor.reisz@gmail.com>
2019-04-17 12:40:07 +0200

    Sharpie modified

--- week-03/day-03/src/Sharpie.java
+++ week-03/day-03/src/Sharpie.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Sharpie {
+  String color;
+  double width;
+  double inkAmount;
+  
+  public static void main(String[] args) {
+  
+  }
+  
+  public void use() {
+    inkAmount--;
+  }
+  
 }
