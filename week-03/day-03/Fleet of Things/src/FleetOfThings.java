75f3acbc936d79cb0a4cf4f736d52d3a896e13ca
greisz <gabor.reisz@gmail.com>
2019-04-17 14:57:35 +0200

    FleetOfThings done

--- week-03/day-03/Fleet of Things/src/FleetOfThings.java
+++ week-03/day-03/Fleet of Things/src/FleetOfThings.java
@@ -7,6 +7,18 @@ public class FleetOfThings {
     // 2. [ ] Remove the obstacles
     // 3. [x] Stand up
     // 4. [x] Eat lunch
+    Thing thing1 = new Thing("Get milk");
+    Thing thing2 = new Thing("Remove the obstacles");
+    Thing thing3 = new Thing("Stand up");
+    Thing thing4 =new Thing("Eat lunch");
+    
+    fleet.add(thing1);
+    fleet.add(thing2);
+    fleet.add(thing3);
+    fleet.add(thing4);
+    
+    thing3.complete();
+    thing4.complete();
     
     System.out.println(fleet);
   }
