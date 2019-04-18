39ea0bdcfeb3d877e7b08df7b1c0048037312472
greisz <gabor.reisz@gmail.com>
2019-04-18 12:17:28 +0200

    feat(Pirates): add ship score

--- week-03/day-03/Pirates/src/Ship.java
+++ week-03/day-03/Pirates/src/Ship.java
@@ -16,16 +16,18 @@ public class Ship {
     this.crewInfo();
   }
   
-  public void captainInfo() {
+  public int captainInfo() {
     if (this.captain != null) {
       System.out.println("Captain's name: " + this.captain.getName());
       System.out.println("The captain has drunk " + this.captain.getDrunkLevel() + " bottles ot rum.");
+      return this.captain.getDrunkLevel();
     } else {
       System.out.println(this + " has no captain, hire somebody!");
+      return 0;
     }
   }
   
-  public void crewInfo() {
+  public int crewInfo() {
     if (this.crew != null) {
       int crewAlive = 0;
       for (int i = 0; i < this.crew.size(); i++) {
@@ -34,9 +36,16 @@ public class Ship {
         }
       }
       System.out.println("Number of alive crew members: " + crewAlive);
+      return crewAlive;
     } else {
       System.out.println(this + " has no crew, hire some pirates!");
+      return 0;
     }
+  }
+  
+  public int score(){
+    int score = this.crewInfo() - this.captainInfo();
+    return score;
   }
   
   public void fillShip(String captainsName) {
