4e69f47c2777d10caa74241e719742cd50104d27
greisz <gabor.reisz@gmail.com>
2019-04-23 11:08:08 +0200

    feat(GreenFoxOrganization): Mentor class

--- week-04/day-02/GreenFoxOrganization/src/Mentor.java
+++ week-04/day-02/GreenFoxOrganization/src/Mentor.java
@@ -1,2 +1,26 @@
-public class Mentor {
+public class Mentor extends Person {
+  String level;
+  
+  public Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
+  
+  public Mentor() {
+    super();
+    this.level = "intermediate";
+  }
+  
+  @Override
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+  
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + name +
+            " , a " + age + " year old " +
+            gender + " " +
+            level + " mentor.");
+  }
 }
