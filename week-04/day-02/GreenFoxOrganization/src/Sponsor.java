4752de9432bf7632771c1afbc51e7d0463461664
greisz <gabor.reisz@gmail.com>
2019-04-23 11:18:11 +0200

    feat(GreenFoxOrganization): Sponsor class

--- week-04/day-02/GreenFoxOrganization/src/Sponsor.java
+++ week-04/day-02/GreenFoxOrganization/src/Sponsor.java
@@ -1,2 +1,34 @@
-public class Sponsor {
+public class Sponsor extends Person {
+  String company;
+  int hiredStudents;
+  
+  public Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+    this.hiredStudents = 0;
+  }
+  
+  public Sponsor() {
+    super();
+    this.company = "Google";
+    this.hiredStudents = 0;
+  }
+  
+  public void hire() {
+    this.hiredStudents++;
+  }
+  
+  @Override
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+  
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + name +
+            ", a " + age + " year old " +
+            gender +
+            " who represents " + company +
+            " and hired " + hiredStudents + " students so far.");
+  }
 }
