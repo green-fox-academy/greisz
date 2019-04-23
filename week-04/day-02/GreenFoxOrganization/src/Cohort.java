542082678e8f196de8f691a95a3b8103f5b7c190
greisz <gabor.reisz@gmail.com>
2019-04-23 11:31:39 +0200

    feat(GreenFoxorganization): Cohort class

--- week-04/day-02/GreenFoxOrganization/src/Cohort.java
+++ week-04/day-02/GreenFoxOrganization/src/Cohort.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cohort {
+  String name;
+  List<Student> studentList;
+  List<Mentor> mentorList;
+  
+  public Cohort(String name) {
+    this.name = name;
+    this.studentList = new ArrayList<>();
+    this.mentorList = new ArrayList<>();
+  }
+  
+  public void addStudent(Student newStudent) {
+    this.studentList.add(newStudent);
+  }
+  
+  public void addMentor(Mentor newMentor) {
+    this.mentorList.add(newMentor);
+  }
+  
+  public void info() {
+    System.out.println("The " + this.name + " cohort has " +
+            this.studentList.size() + " students, and " +
+            this.mentorList.size() + " mentors.");
+  }
 }
