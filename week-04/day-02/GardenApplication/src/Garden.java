5e5a2073e74fe2e1cbbd24efa7c80f3309f36d26
greisz <gabor.reisz@gmail.com>
2019-04-23 12:48:01 +0200

    feat(GardenApplication): add new garden with plants

--- week-04/day-02/GardenApplication/src/Garden.java
+++ week-04/day-02/GardenApplication/src/Garden.java
@@ -3,5 +3,7 @@ import java.util.List;
 public class Garden {
   private List<Plant> plants;
   
-  
+  public void addPlant(Plant newPlant) {
+    this.plants.add(newPlant);
+  }
 }
--- week-04/day-02/GardenApplication/src/Main.java
+++ week-04/day-02/GardenApplication/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
   public static void main(String[] args) {
-  
+    Flower yellowFlower = new Flower("yellow", 5);
+    Flower blueFlower = new Flower("blue", 5);
+    Tree purpleTree = new Tree("purple", 10);
+    Tree orangeTree = new Tree("orange", 20);
+    
+    Garden firstGarden = new Garden();
+    firstGarden.addPlant(yellowFlower);
+    firstGarden.addPlant(blueFlower);
+    firstGarden.addPlant(purpleTree);
+    firstGarden.addPlant(orangeTree);
+    
+    
   }
 }
