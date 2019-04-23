15984a55cc879b3dc1a708a57abf7efd4c1a90ec
greisz <gabor.reisz@gmail.com>
2019-04-23 14:54:58 +0200

    feat(GardenApplication): set parameters in Main for required output

--- week-04/day-02/GardenApplication/src/Main.java
+++ week-04/day-02/GardenApplication/src/Main.java
@@ -1,16 +1,23 @@
 public class Main {
   public static void main(String[] args) {
-    Flower yellowFlower = new Flower("yellow", 5);
-    Flower blueFlower = new Flower("blue", 5);
-    Tree purpleTree = new Tree("purple", 10);
-    Tree orangeTree = new Tree("orange", 20);
+    Flower yellowFlower = new Flower("yellow", 0);
+    Flower blueFlower = new Flower("blue", 0);
+    Tree purpleTree = new Tree("purple", 0);
+    Tree orangeTree = new Tree("orange", 0);
     
     Garden firstGarden = new Garden();
+    
     firstGarden.addPlant(yellowFlower);
     firstGarden.addPlant(blueFlower);
     firstGarden.addPlant(purpleTree);
     firstGarden.addPlant(orangeTree);
     
+    firstGarden.gardenWaterInfo();
     
+    firstGarden.wateringGarden(40);
+    firstGarden.gardenWaterInfo();
+    
+    firstGarden.wateringGarden(70);
+    firstGarden.gardenWaterInfo();
   }
 }
