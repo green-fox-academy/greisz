ae33d483abecb9a2ce822e2e0930a1e2b8d5206d
greisz <gabor.reisz@gmail.com>
2019-04-24 16:25:41 +0200

    feat(Zoo): add kinds of animals

---
+++ week-04/day-03/Zoo/src/Mammal.java
@@ -0,0 +1,6 @@
+public class Mammal extends Animal {
+  @Override
+  public void breed() {
+    System.out.println("pushing miniature versions out");
+  }
+}
---
+++ week-04/day-03/Zoo/src/Reptile.java
@@ -0,0 +1,6 @@
+public class Reptile extends Animal {
+  @Override
+  public void breed() {
+    System.out.println("laying eggs");
+  }
+}
