af999ad67831f8a67c856fb93a1e09793e28a581
greisz <gabor.reisz@gmail.com>
2019-04-24 16:16:09 +0200

    feat(Zoo): add Animal superclass

---
+++ week-04/day-03/Zoo/src/Animal.java
@@ -0,0 +1,11 @@
+public abstract class Animal {
+  private String name;
+  private int age;
+  private String gender;
+  
+  public String getName() {
+    return name;
+  }
+  
+  public abstract void breed();
+}
