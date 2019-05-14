8bfbb0f328468558857b19d078cbcf19750c67db
greisz <gabor.reisz@gmail.com>
2019-05-14 12:53:08 +0200

    feat(StreamExercises): add exercise7

---
+++ week-06/day-02/src/Exercise7.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise7 {
+  public static void main(String[] args) {
+    List<String> cities = Arrays.asList(
+            "ROME",
+            "LONDON",
+            "NAIROBI",
+            "CALIFORNIA",
+            "ZURICH",
+            "NEW DELHI",
+            "AMSTERDAM",
+            "ABU DHABI",
+            "PARIS");
+    
+    cities.stream()
+            .filter(city -> city.startsWith("N"))
+            .forEach(System.out::println);
+  }
+}
