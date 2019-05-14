435bdfee1e1ef329f04e067d6d16488bffcc4b93
greisz <gabor.reisz@gmail.com>
2019-05-14 12:03:01 +0200

    feat(StreamExercises): add exercise4

---
+++ week-06/day-02/src/Exercise4.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise4 {
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    
+    numbers.stream()
+            .mapToInt(i -> i)
+            .average()
+            .ifPresent(avg -> System.out.println("Average is " + Math.round(avg * 100) / 100.0));
+  }
+}
