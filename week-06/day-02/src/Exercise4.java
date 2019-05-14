2a05b77245e16ae22dffea800660623289373be6
greisz <gabor.reisz@gmail.com>
2019-05-14 12:23:15 +0200

    fix(StreamExercises): calculate average of the odd numbers in exercise4

--- week-06/day-02/src/Exercise4.java
+++ week-06/day-02/src/Exercise4.java
@@ -6,6 +6,7 @@ public class Exercise4 {
     List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
     
     numbers.stream()
+            .filter(n -> n % 2 != 0)
             .mapToInt(i -> i)
             .average()
             .ifPresent(avg -> System.out.println("Average is " + Math.round(avg * 100) / 100.0));
