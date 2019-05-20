1fdb9dcb173fa2ba29e16e91297abd816801f2f9
greisz <gabor.reisz@gmail.com>
2019-05-20 12:31:44 +0200

    feat(BankOfSimba): add BankAccount class

---
+++ week-07/day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Models/BankAccount.java
@@ -0,0 +1,23 @@
+package com.greenfoxacademy.bankofsimba.Models;
+
+public class BankAccount {
+  private String name;
+  private double balance;
+  private String animalType;
+  
+  
+  public BankAccount() {
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public double getBalance() {
+    return balance;
+  }
+  
+  public String getAnimalType() {
+    return animalType;
+  }
+}
