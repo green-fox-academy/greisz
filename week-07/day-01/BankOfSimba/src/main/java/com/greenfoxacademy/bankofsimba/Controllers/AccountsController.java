437cf77e686caa3d350138e84ab68cdaad769a59
greisz <gabor.reisz@gmail.com>
2019-05-20 16:30:59 +0200

    feat(BankOfSimba): add king and goodGuy properties

--- week-07/day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Controllers/AccountsController.java
+++ week-07/day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Controllers/AccountsController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.bankofsimba.Controllers;
 import com.greenfoxacademy.bankofsimba.Models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,25 +14,24 @@ import java.util.List;
 public class AccountsController {
   private List<BankAccount> accountsList;
   
+  @ModelAttribute
+  public void setup(Model model) {
+    accountsList = new ArrayList<>();
+    accountsList.add(new BankAccount("Simba", 1000, "lion", true, true));
+    accountsList.add(new BankAccount("Timon", 800, "suricate", false, true));
+    accountsList.add(new BankAccount("Pumbaa", 900, "warthog", false, true));
+    accountsList.add(new BankAccount("Scar", 1000, "lion", false, false));
+    accountsList.add(new BankAccount("Mufasa", 1000, "lion", true, true));
   
+  }
   @RequestMapping(path = "/show", method = RequestMethod.GET)
   public String showAccounts(Model model) {
-    accountsList = new ArrayList<>();
-    accountsList.add(new BankAccount("Simba", 1000, "lion"));
-    accountsList.add(new BankAccount("Timon", 800, "suricate"));
-    accountsList.add(new BankAccount("Pumbaa", 900, "warthog"));
-    
     model.addAttribute("accounts", accountsList);
     return "accounts";
   }
   
   @RequestMapping(path = "/showTable", method = RequestMethod.GET)
   public String showAccountsInTable(Model model) {
-    accountsList = new ArrayList<>();
-    accountsList.add(new BankAccount("Simba", 1000, "lion"));
-    accountsList.add(new BankAccount("Timon", 800, "suricate"));
-    accountsList.add(new BankAccount("Pumbaa", 900, "warthog"));
-    
     model.addAttribute("accounts", accountsList);
     return "accounts_table";
   }
--- week-07/day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Models/BankAccount.java
+++ week-07/day-01/BankOfSimba/src/main/java/com/greenfoxacademy/bankofsimba/Models/BankAccount.java
@@ -4,15 +4,19 @@ public class BankAccount {
   private String name;
   private double balance;
   private String animalType;
+  private boolean king;
+  private boolean goodGuy;
   
   
   public BankAccount() {
   }
   
-  public BankAccount(String name, double balance, String animalType) {
+  public BankAccount(String name, double balance, String animalType, boolean king, boolean goodGuy) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
+    this.king = king;
+    this.goodGuy = goodGuy;
   }
   
   public String getName() {
@@ -25,5 +29,13 @@ public class BankAccount {
   
   public String getAnimalType() {
     return animalType;
+  }
+  
+  public boolean isKing() {
+    return king;
+  }
+  
+  public boolean isGoodGuy() {
+    return goodGuy;
   }
 }
