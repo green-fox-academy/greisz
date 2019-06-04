29a2be194d008738263398a6e718021c36e4e817
greisz <gabor.reisz@gmail.com>
2019-06-04 12:29:32 +0200

    feat(Rest testing): init project

---
+++ week-09/day-02/src/main/java/com/greenfoxacademy/resttesting/ResttestingApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.resttesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttestingApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(ResttestingApplication.class, args);
+  }
+  
+}
